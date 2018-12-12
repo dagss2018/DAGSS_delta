4bb59d35e4ae7ad39e1eeb4e66089b4adc55e87e
jnseoane <jnseoane@esei.uvigo.es>
2018-12-12 19:56:09 +0100

    Prototipo vista

---
+++ src/main/java/es/uvigo/esei/dagss/controladores/medico/GestionCitasControlador.java
@@ -0,0 +1,15 @@
+package es.uvigo.esei.dagss.controladores.medico;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Juan
+ */
+public class GestionCitasControlador {
+    
+}
